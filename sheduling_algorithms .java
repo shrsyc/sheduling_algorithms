143e4d73d5380fb383291458f376a7d9b3ab5b85
shrsyc <shrsyc@gmail.com>
2022-12-03 14:44:54 +0530

    first commit

---
+++ sheduling_algorithms .java
@@ -0,0 +1,35 @@
+import javax.swing.*;  
+import java.awt.*;
+import java.awt.event.*;
+
+class open implements ActionListener{
+    JLabel jl;
+    open(){
+        JFrame jf=new JFrame("sheduling_algorithms");
+        jf.setLayout(new FlowLayout());
+        jf.setSize(1000,600);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton b1=new JButton("ALPHA");
+        JButton b2=new JButton("BETA");
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        jf.add(b1);
+        jf.add(b2);
+        jl=new JLabel("press button");
+        jf.add(jl);
+        jf.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getActionCommand()=="ALPHA")
+            jl.setText("alpha was pressed");
+        if(ae.getActionCommand()=="BETA")
+            jl.setText("beta was pressed");    
+    }
+    public static void main(String args[]){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                new open();
+            }
+        });
+    }
+}
